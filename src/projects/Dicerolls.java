291c414b0cbe1c1771f790dc8fd3510e11caa217
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-01-31 12:18:33 +0200

    roll_dice function

--- src/projects/Dicerolls.java
+++ src/projects/Dicerolls.java
@@ -1,10 +1,31 @@
 package projects;
-
+import java.util.ArrayList;
+import java.util.Random;
 public class Dicerolls {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public static ArrayList roll_dice(int amount) {
+		if (amount < 0) {
+            throw new IllegalArgumentException("Value must be non-negative");
+        }
+		
+		Random random = new Random();
+		ArrayList<Integer> rolls = new ArrayList<>();
+		try {
+            
+            while(amount > 0) {
+            	int random_roll = random.nextInt(6) + 1;
+            	rolls.add(random_roll);
+            	amount --;
+            } 
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            throw e;
+        }
+		return rolls;
+	}
 }
