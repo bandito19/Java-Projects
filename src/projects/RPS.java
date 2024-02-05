0eca17986fe2b344c1a66d27fa1b9bdcd947492a
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-02-05 13:51:08 +0200

    fixing play game method

--- src/projects/RPS.java
+++ src/projects/RPS.java
@@ -5,29 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.random.*;
 
 public class RPS {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to RPS!");
+	public static void play_game(String user_move) {
 		Map<String, String> moves = new HashMap<>();
 		moves.put("rock" , "🌑");
 		moves.put("paper","📜");
 		moves.put("scissors", "✂");
 		List<String> validMoves = new ArrayList<>(moves.keySet());
 		
-		
-	}
-	
-	public static void play_game() {
-		Scanner inp = new Scanner(System.in);
-		System.out.print("Rock, paper or scissors: ");
-		String user_move = inp.nextLine();
 		if (!validMoves.contains(user_move)) {
 			System.out.println("Invalid move.");
 			return;
 		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to RPS!");
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Rock, paper or scissors: ");
+		String user_move = inp.nextLine();
+		
+	
+	
+	
 	}
 
 }
