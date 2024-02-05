d1f1683668f9f61544d0d6e95a3058e5d0bad27b
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-02-05 14:11:15 +0200

    check moves method

--- src/projects/RPS.java
+++ src/projects/RPS.java
@@ -7,10 +7,10 @@ import java.util.Map;
 import java.util.Random;
 
 public class RPS {
-	Map<String, String> moves = new HashMap<>();
-	List<String> validMoves = new ArrayList<>(moves.keySet());
+	static Map<String, String> moves = new HashMap<>();
+	static List<String> validMoves = new ArrayList<>(moves.keySet());
 	
-	public void play_game(String user_move) {
+	public static void play_game(String user_move) {
 		
 		moves.put("rock" , "🌑");
 		moves.put("paper","📜");
@@ -24,7 +24,7 @@ public class RPS {
 		
 		
 	}
-	public String ai_move() {
+	public static String ai_move() {
 		Random random = new Random();
 		int rand_index = random.nextInt(3);
 		return validMoves.get(rand_index);
@@ -35,6 +35,23 @@ public class RPS {
 		System.out.println("You " + moves.get(ai_move));
 		System.out.println("----");
 	}
+	public void checkMoves(String user_move, String ai_move) {
+		if (user_move == ai_move) {
+			System.out.println("It is a tie.");
+		}
+		else if (user_move == "rock" && ai_move == "scissors") {
+			System.out.println("You win!");
+		}
+		else if (user_move == "scissors" && ai_move == "paper") {
+			System.out.println("You win!");
+		}
+		else if (user_move == "paper" && ai_move == "rock") {
+			System.out.println("You win!");
+		}
+		else {
+			System.out.println("Ai wins!");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -42,10 +59,8 @@ public class RPS {
 		Scanner inp = new Scanner(System.in);
 		System.out.print("Rock, paper or scissors: ");
 		String user_move = inp.nextLine();
-		
-	
-	
-	
+		while (true) {
+			play_game(user_move);
+		}
 	}
-
 }
