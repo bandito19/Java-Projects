78a1e47d98c4fccca88b688006e533876fafca5c
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-02-05 13:58:50 +0200

    ai move method

--- src/projects/RPS.java
+++ src/projects/RPS.java
@@ -1,25 +1,30 @@
 package projects;
 import java.util.Scanner;
-import java.util.random.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.random.*;
+import java.util.Random;
 
 public class RPS {
-	public static void play_game(String user_move) {
-		Map<String, String> moves = new HashMap<>();
+	Map<String, String> moves = new HashMap<>();
+	List<String> validMoves = new ArrayList<>(moves.keySet());
+	public void play_game(String user_move) {
+		
 		moves.put("rock" , "🌑");
 		moves.put("paper","📜");
 		moves.put("scissors", "✂");
-		List<String> validMoves = new ArrayList<>(moves.keySet());
 		
 		if (!validMoves.contains(user_move)) {
 			System.out.println("Invalid move.");
 			return;
 		}
 		
+	}
+	public String ai_move() {
+		Random random = new Random();
+		int rand_index = random.nextInt(3);
+		return validMoves.get(rand_index);
 	}
 
 	public static void main(String[] args) {
